9ce5ff510f0ae22cc58f6b7a4225104df38bdf5d
acfoundry <acfoundry@users.noreply.github.com>
2026-09-22 14:57:54 +0000

    deploy: a100af92bd8600a323f16e3767f1e5a65b25e2ea

---
+++ assets/swimlanesDiagram-TC7HE7FX-Lpw7sUez.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-KWPJA3E3-CH4o8ALs.js";import{_ as a}from"./index-BinZPLr3.js";import"./chunk-5VM5RSS4-B4bSoB5A.js";import"./chunk-XXDRQBXY-BZdLWwP7.js";import"./chunk-WEXAMYUT-139YoHmj.js";import"./chunk-GWA4HPMP-DbO4alC1.js";import"./channel-DqWdog0f.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster:not([data-color-id]) rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=r({styles:s});export{f as diagram};
